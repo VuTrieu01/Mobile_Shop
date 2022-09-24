be29be0a3ad4f0a7eca475ec2137270810ce99e7
auhai30032001 <auhai30032001@gmail.com>
2022-09-24 08:29:42 +0700

    .
    page apple

--- src/features/apple/Apple.js
+++ src/features/apple/Apple.js
@@ -1,7 +1,8 @@
 import * as React from "react";
-import img from "../../assets/images/sale-phone.webp";
-import product from "../../assets/images/product1.webp";
+import product2 from "../../assets/images/product1.webp";
+import product from "../../assets/images/5.jpg";
 import { Link } from "react-router-dom";
+import { MdPhoneIphone } from "react-icons/md";
 export default function Apple() {
   return (
     <>
@@ -10,129 +11,97 @@ export default function Apple() {
         <div className="container__box">
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
           <div className="container__box--item">
             <div className="item">
-              <Link to="/">
-                <img className="img" src={img} alt="" />
-              </Link>
+              <MdPhoneIphone size={75} />
             </div>
             <span>iPhone</span>
           </div>
@@ -143,20 +112,173 @@ export default function Apple() {
         <div className="product__container--items">
           <div className="product__item">
             <Link to="/">
-              <img className="product__img" src={product} alt="" />
+              <img className="product__img" src={product2} alt="" />
             </Link>
-            <span>iPhone14Promax</span>
+            <h3>iPhone 14 Promax</h3>
             <div className="type">
-              <button>128gb</button>
-              <button>128gb</button>
-              <button>128gb</button>
-              <button>128gb</button>
+              <button className="button active">128GB</button>
+              <button className="button">
+                <p>256GB</p>
+              </button>
+              <button className="button">
+                <p>512GB</p>
+              </button>
+              <button className="button">
+                <p>1TB</p>
+              </button>
             </div>
-            <span>30.900.900</span>
+            <h2>30.900.900 đ</h2>
             <div className="desc__item">
-              <span>Apple 16 Bionic</span>
-              <span>6.1 inch</span>
+              <span>Apple 16 Bionic |</span>
+              <span>6.1 inch |</span>
               <span>128 gb</span>
+            </div>
+            <div className="form-button">
+              <button className="btn-buy">
+                <i>Mua ngay</i>
+              </button>
+              <button className="btn-compare">
+                <i>So sánh</i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="product__container--items">
+          <div className="product__item">
+            <Link to="/">
+              <img className="product__img" src={product2} alt="" />
+            </Link>
+            <h3>iPhone 14 Promax</h3>
+            <div className="type">
+              <button className="button active">128GB</button>
+              <button className="button">
+                <p>256GB</p>
+              </button>
+              <button className="button">
+                <p>512GB</p>
+              </button>
+              <button className="button">
+                <p>1TB</p>
+              </button>
+            </div>
+            <h2>30.900.900 đ</h2>
+            <div className="desc__item">
+              <span>Apple 16 Bionic |</span>
+              <span>6.1 inch |</span>
+              <span>128 gb</span>
+            </div>
+            <div className="form-button">
+              <button className="btn-buy">
+                <i>Mua ngay</i>
+              </button>
+              <button className="btn-compare">
+                <i>So sánh</i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="product__container--items">
+          <div className="product__item">
+            <Link to="/">
+              <img className="product__img" src={product2} alt="" />
+            </Link>
+            <h3>iPhone 14 Promax</h3>
+            <div className="type">
+              <button className="button active">128GB</button>
+              <button className="button">
+                <p>256GB</p>
+              </button>
+              <button className="button">
+                <p>512GB</p>
+              </button>
+              <button className="button">
+                <p>1TB</p>
+              </button>
+            </div>
+            <h2>30.900.900 đ</h2>
+            <div className="desc__item">
+              <span>Apple 16 Bionic |</span>
+              <span>6.1 inch |</span>
+              <span>128 gb</span>
+            </div>
+            <div className="form-button">
+              <button className="btn-buy">
+                <i>Mua ngay</i>
+              </button>
+              <button className="btn-compare">
+                <i>So sánh</i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="product__container--items">
+          <div className="product__item">
+            <Link to="/">
+              <img className="product__img" src={product2} alt="" />
+            </Link>
+            <h3>iPhone 14 Promax</h3>
+            <div className="type">
+              <button className="button active">128GB</button>
+              <button className="button">
+                <p>256GB</p>
+              </button>
+              <button className="button">
+                <p>512GB</p>
+              </button>
+              <button className="button">
+                <p>1TB</p>
+              </button>
+            </div>
+            <h2>30.900.900 đ</h2>
+            <div className="desc__item">
+              <span>Apple 16 Bionic |</span>
+              <span>6.1 inch |</span>
+              <span>128 gb</span>
+            </div>
+            <div className="form-button">
+              <button className="btn-buy">
+                <i>Mua ngay</i>
+              </button>
+              <button className="btn-compare">
+                <i>So sánh</i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="product__container--items">
+          <div className="product__item">
+            <Link to="/">
+              <img className="product__img" src={product2} alt="" />
+            </Link>
+            <h3>iPhone 14 Promax</h3>
+            <div className="type">
+              <button className="button active">128GB</button>
+              <button className="button">
+                <p>256GB</p>
+              </button>
+              <button className="button">
+                <p>512GB</p>
+              </button>
+              <button className="button">
+                <p>1TB</p>
+              </button>
+            </div>
+            <h2>30.900.900 đ</h2>
+            <div className="desc__item">
+              <span>Apple 16 Bionic |</span>
+              <span>6.1 inch |</span>
+              <span>128 gb</span>
+            </div>
+            <div className="form-button">
+              <button className="btn-buy">
+                <i>Mua ngay</i>
+              </button>
+              <button className="btn-compare">
+                <i>So sánh</i>
+              </button>
             </div>
           </div>
         </div>
--- src/features/user/SignUp.js
+++ src/features/user/SignUp.js
@@ -34,7 +34,7 @@ export default function Signup({ register, showRegister, showLogin }) {
             <AiFillCloseCircle
               size={25}
               className="login__fields--icon"
-              onClick={(showLogin, showRegister)}
+              onClick={showRegister}
             />
             <form className="login__fields--item" onSubmit={handleSubmit}>
               <h1>Đăng ký</h1>
