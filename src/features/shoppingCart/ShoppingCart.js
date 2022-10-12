0847ada7641beba101a398efc66eb3b22945d4d2
VuTrieu01 <vutrieu815@gmail.com>
2022-10-12 23:25:15 +0700

    create page cart

--- src/features/shoppingCart/ShoppingCart.js
+++ src/features/shoppingCart/ShoppingCart.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
+import { useRef } from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { TiTick } from "react-icons/ti";
 import dongho from "../../assets/images/dongho.webp";
 
 export default function ShoppingCart() {
-  const [money, setMoney] = useState(50000);
-  let handleQuantity = (evt) => {
-    setMoney(50000 * evt.target.value);
-  };
+  const quantity = useRef();
+  function totalPrice(price, total) {
+    return price * total;
+  }
 
   return (
     <div className="cart">
@@ -15,74 +16,48 @@ export default function ShoppingCart() {
         <h2>THÔNG TIN GIỎ HÀNG</h2>
         <div className="cart__fields--tb">
           <table>
-            <tr>
-              <th colSpan={2}>THÔNG TIN SẢN PHẨM</th>
-              <th>SỐ LƯỢNG</th>
-              <th>THÀNH TIỀN</th>
-              <th>CÔNG CỤ</th>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src={dongho}
-                  alt=""
-                  className="cart__fields--tb--image"
-                ></img>
-              </td>
-              <td className="cart__fields--tb--text">Đồng hồ</td>
-              <td>
-                <input
-                  type="number"
-                  name="quantity"
-                  min="1"
-                  max="10"
-                  defaultValue={1}
-                  onChange={handleQuantity}
-                />
-              </td>
-              <td>
-                <h4>{money}đ</h4>
-              </td>
-              <td>
-                <button>
-                  <AiFillDelete />
-                  Xóa
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  src={dongho}
-                  alt=""
-                  className="cart__fields--tb--image"
-                ></img>
-              </td>
-              <td className="cart__fields--tb--text">Đồng hồ</td>
-              <td>
-                <input
-                  type="number"
-                  name="quantity"
-                  min="1"
-                  max="10"
-                  defaultValue={1}
-                  onChange={handleQuantity}
-                />
-              </td>
-              <td>
-                <h4>{money}đ</h4>
-              </td>
-              <td>
-                <button>
-                  <AiFillDelete />
-                  Xóa
-                </button>
-              </td>
-            </tr>
+            <thead>
+              <tr>
+                <th>THÔNG TIN SẢN PHẨM</th>
+                <th>SỐ LƯỢNG</th>
+                <th>THÀNH TIỀN</th>
+                <th>CÔNG CỤ</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="cart__fields--tb--item">
+                  <img src={dongho} alt="" />
+                  <span>Đồng hồ</span>
+                </td>
+
+                <td>
+                  {/* <span>a</span> */}
+                  <input
+                    type="number"
+                    name="quantity"
+                    min="1"
+                    max="10"
+                    defaultValue={1}
+                  />
+                  {/* <span>-</span> */}
+                </td>
+                <td>
+                  <h4>đ</h4>
+                </td>
+                <td>
+                  <button>
+                    <AiFillDelete />
+                    Xóa
+                  </button>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="cart__fields--item">
-          <h3>Tổng tiền: {money}</h3>
+          <h3>Tổng tiền: </h3>
           <h4>
             <AiFillDelete /> Xóa tất cả sản phẩm trong Giỏ hàng
           </h4>
